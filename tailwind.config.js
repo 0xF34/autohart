3a7c0ba97bf8324d5808681f5651a0db2dd3181c
unknown <tempmailorg@yahoo.com>
2026-08-22 18:44:13 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './app/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}'
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
